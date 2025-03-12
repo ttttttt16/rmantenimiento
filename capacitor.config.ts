a26b4701d8e10399a508f6f6458be733a0ae296d
tabatanovillo239 <tabatanovillo239@gmail.com>
2025-03-12 17:19:10 +0000

    IOSActualizado

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.tabataMantenimiento.app',
   appName: 'RMantenimiento',
-  webDir: 'dist'
+  webDir: 'dist/myapp/browser'
 };
 
 export default config;
